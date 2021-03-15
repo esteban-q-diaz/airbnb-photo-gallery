0583643299a675e333049241063790319f099780
esteban-q-diaz <estebandpal@gmail.com>
2021-03-14 23:19:59 -0700

    minor css changes

--- client/components/PhotoGallery.jsx
+++ client/components/PhotoGallery.jsx
@@ -10,8 +10,8 @@ function PhotoGallery() {
       <div  class='imageContainer'>
 
 
-        <div class="imageFlex">
-          <img src="https://a0.muscache.com/im/pictures/dcde8a25-b336-484a-ab6d-f13d4f93c60e.jpg?im_w=960" height="530" width="450"/>
+        <div class="imageFlex ">
+          <img class="leftImg" src="https://a0.muscache.com/im/pictures/dcde8a25-b336-484a-ab6d-f13d4f93c60e.jpg?im_w=960" height="530" width="450"/>
         </div>
 
 <div class="one">
@@ -21,17 +21,17 @@ function PhotoGallery() {
 
 
         <div class="imageFlex">
-          <img src="https://a0.muscache.com/im/pictures/miso/Hosting-42894626/original/3ec1ad2d-f82f-4d54-bba9-effa5e5e3112.jpeg?im_w=720" width="350"/>
+          <img src="https://a0.muscache.com/im/pictures/miso/Hosting-42894626/original/6139003d-d69a-40c9-814c-288944c2c94d.jpeg?im_w=720" width="350"/>
         </div>
 </div>
 
 <div class="two">
         <div class="imageFlex">
-          <img src="https://a0.muscache.com/im/pictures/miso/Hosting-42894626/original/6139003d-d69a-40c9-814c-288944c2c94d.jpeg?im_w=720" width="350"/>
+          <img class="rightTopImg" src="https://a0.muscache.com/im/pictures/miso/Hosting-42894626/original/6139003d-d69a-40c9-814c-288944c2c94d.jpeg?im_w=720" width="350"/>
         </div>
 
         <div class="imageFlex">
-            <img src="https://a0.muscache.com/im/pictures/miso/Hosting-42894626/original/3ec1ad2d-f82f-4d54-bba9-effa5e5e3112.jpeg?im_w=720" width="350"/>
+            <img class="rightBottomImg" src="https://a0.muscache.com/im/pictures/miso/Hosting-42894626/original/3ec1ad2d-f82f-4d54-bba9-effa5e5e3112.jpeg?im_w=720" width="350"/>
         </div>
 </div>
       </div>
