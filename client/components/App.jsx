893bb4bd564892806c55140ff1d3c1e165a1545a
esteban-q-diaz <estebandpal@gmail.com>
2021-03-21 17:26:35 -0700

    server update and slow connection fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,8 @@ import NavBar from './NavBar'
 import SearchMenu from './SearchMenu';
 import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom';
 import Images from './Images';
-import details from '/database/sample-data.js'
+import details from '/database/sample-data.js';
+import axios from 'axios';
 
 function App() {
   const [searchMenu, setSearchMenu] = useState(false);
@@ -15,6 +16,10 @@ function App() {
   // useEffect(() => {
   //   console.log(details)
   // })
+
+  // axios.get('http://localhost:3030/getdetails')
+  //   .then(()=> console.log('we got it'))
+  //   .catch(()=> console.log('we dont got it'))
 
   const showMenu = () => {
     console.log('i am here')
@@ -34,13 +39,13 @@ function App() {
   return (
 
     <div>
-        {imageGallery ? <Images showGallery={showGallery}/> :  null }
+        {/* {imageGallery ? <Images showGallery={showGallery}/> :  null }
         {searchMenu ? <SearchMenu /> : <NavBar showMenu={showMenu}/>}
         <Header details={details}/>
-        <PhotoGallery searchMenu={searchMenu} setModal={setModal} showGallery={showGallery}/>
+        <PhotoGallery searchMenu={searchMenu} setModal={setModal} showGallery={showGallery}/> */}
 
 
-{/* <Router>
+<Router>
       <Switch>
         <Route path="/images" exact>
           <Images />
@@ -48,12 +53,12 @@ function App() {
 
         <Route path="/" exact>
           {searchMenu ? <SearchMenu /> : <NavBar showMenu={showMenu}/>}
-          <Header />
+          <Header details={details}/>
           <PhotoGallery setModal={setModal} />
         </Route>
 
       </Switch>
-  </Router> */}
+  </Router>
 </div>
 
   )
--- server/server.js
+++ server/server.js
@@ -1,25 +1,35 @@
-// const express = require('express')
-// const PORT = 3030;
-// const path = require('path')
-// const app = express();
+const express = require('express')
+const PORT = 3000;
+const path = require('path')
+const app = express();
+var bodyParser = require('body-parser')
 
+//this is needed to make the public folder available to the server
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
-// app.use(express.static('public', { maxAge: 31557600 }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// // app.use(express.static(__dirname + '/public'));
+// parse application/json
+app.use(bodyParser.json())
 
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname, '../public/index.html'), function(err) {
-//     if (err) {
-//       res.status(500).send(err)
-//     }
-//   })
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+// app.get('/getdetails', function(req, res) {
+//   res.send(202)
 // })
 
-// app.listen(PORT, (err) => {
-//   if (err) {
-//     console.log('error at server', err)
-//   } else {
-//     console.log(`Server running on localhost:${PORT}`)
-//   }
-// })
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('error at server', err)
+  } else {
+    console.log(`Server running on localhost:${PORT}`)
+  }
+})
